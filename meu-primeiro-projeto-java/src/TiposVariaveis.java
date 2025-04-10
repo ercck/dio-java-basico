ae3cbe1c49d96856d92468797a77c71f42549838
Erick Melo <erickmelo.developer@gmail.com>
2025-04-10 17:00:22 -0300

    adicionando tipos variaveis e anotando consultas

--- meu-primeiro-projeto-java/src/TiposVariaveis.java
+++ meu-primeiro-projeto-java/src/TiposVariaveis.java
@@ -2,5 +2,18 @@ public class TiposVariaveis {
     
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        double salarioMedio = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; /*nesse caso seria necessário realizar um casting pois o int pode ser maior que o short no total*/
+
+        /*esse número sempre pode receber um outro valor, só precisa declarar int no inicio*/
+        int numero = 1;
+        /*caso queira que seja uma variavel que nunca mude, escreva final antes da variavel*/
+        numero = 2;
+
+        
     }
 }
